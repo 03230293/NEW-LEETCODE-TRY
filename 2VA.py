e8146b17d265ea263e4bddf770e0d142d1138f0b
03230293 <03230293.gcbs@rub.edu.bt>
2024-01-21 11:32:18 +0600

    2nd try

--- 2VA.py
+++ 2VA.py
@@ -5,7 +5,9 @@ Output=True
 def validAna(s, t):
     for i in s:
         for j in t:
-            if i==j:
+            countS, countT= {},{}
+
+            if countS[s[i]]==countT[t[j]]:
                 print(Output)
             
             else:
